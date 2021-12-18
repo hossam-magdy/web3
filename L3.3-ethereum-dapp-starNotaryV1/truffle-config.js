3d4e8c69a142987d4a86f8be811856d6d367f0f4
Hossam Magdy <hossam.magdy@outlook.com>
2021-12-18 18:41:27 +0100

    Add frontend (html+js) connecting to starNotary app on deployed network
    View functions (not sending tx) + Setting functions (sending tx)
    through RPC (http requests with specific formats, like GraphQL)

--- L3.3-ethereum-dapp-starNotaryV1/truffle-config.js
+++ L3.3-ethereum-dapp-starNotaryV1/truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
+const HDWallet = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -42,14 +42,14 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 9545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
     
     develop: {
-      port: 8545
+      port: 9545
     },
 
     // Another network with more advanced options...

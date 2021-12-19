ba33b57a4f2834800030470a7c80f56e051197ed
Hossam Magdy <hossam.magdy@outlook.com>
2021-12-19 01:03:12 +0100

    Update tests of StarNotaryV2 ERC721: split test

--- L3.3-ethereum-dapp-starNotaryV2-(tokenized)/test/StarNotary.js
+++ L3.3-ethereum-dapp-starNotaryV2-(tokenized)/test/StarNotary.js
@@ -24,7 +24,20 @@ contract('StarNotary', (accounts) => {
     assert.equal(await instance.starsForSale.call(tokenId), price);
   });
 
-  it('enables users to buyStar, and updates balances as expected', async () => {
+  it('lets user2 to buyStar from user1', async () => {
+    const tokenId = 4;
+    const instance = await StarNotary.deployed();
+    await instance.createStar('Awesome Star', tokenId, { from: account1 });
+
+    const price = web3.utils.toWei('.01', 'ether');
+    await instance.putStarUpForSale(tokenId, price, { from: account1 });
+
+    await instance.buyStar(tokenId, { from: account2, value: price });
+
+    assert.equal(await instance.ownerOf(tokenId, { from: account1 }), account2);
+  });
+
+  it('updates balances of user2 after buyStar from user1', async () => {
     const tokenId = 3;
     const instance = await StarNotary.deployed();
     await instance.createStar('Awesome Star', tokenId, { from: account1 });
@@ -35,9 +48,10 @@ contract('StarNotary', (accounts) => {
     const balance1Before = web3.utils.toBN(await web3.eth.getBalance(account1));
     const balance2Before = web3.utils.toBN(await web3.eth.getBalance(account2));
 
+    const value = price.add(web3.utils.toBN(web3.utils.toWei('.01', 'ether'))); // send more than price, assert later that change is returned
     const tx = await instance.buyStar(tokenId, {
       from: account2,
-      value: price,
+      value: value,
     });
     const gasUsed = web3.utils.toBN(tx.receipt.gasUsed);
     const gasPrice = web3.utils.toBN(await web3.eth.getGasPrice());
@@ -47,7 +61,6 @@ contract('StarNotary', (accounts) => {
     const balance1After = web3.utils.toBN(await web3.eth.getBalance(account1));
     const balance2After = web3.utils.toBN(await web3.eth.getBalance(account2));
 
-    assert.equal(await instance.ownerOf(tokenId, { from: account1 }), account2);
     assert.equal(
       balance1Before.add(price).toString(),
       balance1After.toString()

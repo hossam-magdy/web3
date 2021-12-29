f26b457b968c1ad5e414511dd245c0d90a9bd68b
Hossam Magdy <hossam.magdy@outlook.com>
2021-12-29 19:29:54 +0100

    Project Capstone: to proceed in separate repo/submodule
    https://github.com/hossam-magdy/erc721-nft-zokrates-zksnarks

--- Project.Capstone/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-// migrating the appropriate contracts
-var SquareVerifier = artifacts.require("./SquareVerifier.sol");
-var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
-
-module.exports = (deployer) => {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
-};
